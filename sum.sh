7f9b0c38b46027ed23b42183638eab56676cce5a
张新征 <programer.email@gmail.com>
2019-02-12 23:10:45 +0800

    add sum.sh

---
+++ sum.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+
+while true
+do
+  read -p "please input a positive number: " num
+  sum=0
+  expr $num + 1 &> /dev/null
+  if [ $? -eq 0 ];then
+    if [ `expr $num \> 0` -eq 1 ];then
+      for((i=1; i<=$num; i++))
+      do
+        sum=`expr $sum + $i`
+      done
+        echo "1+2+3+...+$num = $sum"
+        exit
+    fi
+  fi
+  echo "error, input enlegal"
+  continue
+done
